d5901b88986bc590ac23430e261b9437f5ac5137
3835689650 <3835689650@users.noreply.github.com>
2026-09-19 16:56:04 +0800

    apt 1.0.41

--- setpath.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set "OLDPATH="
-for /f "skip=2 tokens=2,*" %%a in ('reg query "HKCU\Environment" /v Path 2^>nul') do set "OLDPATH=%%b"
-reg add "HKCU\Environment" /v Path /t REG_EXPAND_SZ /d "%OLDPATH%;%LOCALAPPDATA%\1b1t" /f >nul
